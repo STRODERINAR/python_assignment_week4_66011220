56dfef53f4c07cd2909dec9d788bff4cbcc3b567
STRODERINAR <66011220@kmitl.ac.th>
2024-07-26 10:00:17 +0700

    Create string_operations.py

---
+++ string_operations.py
@@ -0,0 +1,14 @@
+def reverse_string(a):
+    
+    return a[::-1]
+def capitalize_string(a):
+    
+    return a.capitalize()
+def Lowercase_string(a):
+    
+    return a.lower()
+def Uppercase_stiring(a):
+    
+    return a.upper()
+
+print(reverse_string("Hello world"))
