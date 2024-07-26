6947273d1df0877166ef4098cc2756c6f27d6d71
STRODERINAR <66011220@kmitl.ac.th>
2024-07-26 09:58:13 +0700

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    return a+b
+def substract(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    if a == 0 or b==0:
+        print("Division is error")
+    else:
+        return a/b
